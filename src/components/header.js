43dfde2ed09033359775a8b9ca5d0a7e3c35ab9e
David Pineda <david.pineda@deparadise.com>
2021-08-14 20:50:21 -0400

    Add some test styles for header and footer modififcation

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+// styles
+const headerStyles = {
+	border: "1px solid red",
+	height: "200px",
+ }
+
 const Header = ({ someNode }) => {
 	console.log('>> TEST RENDER HEADER:', someNode);
 
 	return (
-		<>
+		<header style={headerStyles}>
 			<h1>HEADER IS_WORKING</h1>
-		</>
+		</header>
 	);
 };
 
